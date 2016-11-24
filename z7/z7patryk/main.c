abc7fc54790ad875e732085d981dfabd17c4cf13
PatrykCholewa <patryk7500@gmail.com>
2016-11-24 02:56:50 +0100

    Już kompiluje, ale nie działa.

---
+++ z7/z7patryk/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+#include "bibl.h"  
+
+/*	
+typedef struct c{
+	int **pozycje;
+	char **slowa;
+	int *liczba_wystapien;
+	int ile_slow;
+} skorowidz_t;
+*/
+
+//void zainicjuj_skorowidz( int argc, char **argv, skorowidz_t skorowidz );
+
+//void szukaj_do_skorowidza( FILE *in, skorowidz_t skorowidz);
+
+//void wypisz_skorowidz(skorowidz_t skorowidz);
+
+int
+main( int argc, char **argv ) {
+
+	FILE *in= argc > 1 ? fopen( argv[1], "r" ) : stdin;
+
+	skorowidz_t *skorowidz;
+
+	zainicjuj_skorowidz( argc, argv, skorowidz);
+
+	if( argc < 2 ) {
+		fprintf( stderr, "%s: błąd: proszę podać słowa do wyszukiwania\n", argv[0] );
+		return EXIT_FAILURE;
+	}
+
+	if( in == NULL ) {
+		fprintf( stderr, "%s: błąd: nie mogę czytać pliku %s\n", argv[0], argv[1] );
+		return EXIT_FAILURE;
+	}
+
+	szukaj_do_skorowidza( in , skorowidz);
+
+	wypisz_skorowidz( skorowidz);
+
+	return EXIT_SUCCESS;
+}
+
