9f4ad4de5daf76e349608f1a6bfcb2ad49439ec9
PatrykCholewa <patryk7500@gmail.com>
2016-11-29 23:18:46 +0100

    Prace trawają...

---
+++ z8/roboczy/dane.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void store_add_def ( char * get_from_fun_stack, int alex_getLN, FILE * inpname);
+
+void store_add proto ( char * get_from_fun_stack, int alex_getLN, FILE * inpname);
+
+void store_add_call ( char * get_from_fun_stack, int alex_getLN, FILE * inpname);
---
+++ z8/roboczy/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "parser.h"
+#include "cout.h"
+//#include "dane.h"
+
+int main ( int argc, char **argv){
+	
