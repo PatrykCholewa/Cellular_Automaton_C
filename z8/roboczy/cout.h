7f2798abfe89062e847bdc0ccc49bc939318e33e
PatrykCholewa <patryk7500@gmail.com>
2016-11-29 23:46:02 +0100

    Prace wciąż trwają. Main.c gotowy.

---
+++ z8/roboczy/cout.h
@@ -0,0 +1,3 @@
+// '#' jeśli potrzebny
+
+void wypisz( int argc_minus_jeden ); // wypisuje na stdout
--- z8/roboczy/main.c
+++ z8/roboczy/main.c
@@ -4,4 +4,25 @@
 //#include "dane.h"
 
 int main ( int argc, char **argv){
+
+	int i;
 	
+	if( argc = 1 ){
+		
+		printf("Podaj plik do czytania!");
+		return EXIT_FAILURE;
+
+	}
+
+	for(i=1 ; i <= argc ; i++ ){
+
+		char *inpname = argv[i];	
+		analizatorSkladni( inpname );
+
+	}
+
+	wypisz( argc - 1 );
+
+	return EXIT_SUCCESS;
+
+}	
