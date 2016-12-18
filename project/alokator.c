a7c46ea651b2c57304045c05dd4726fe4c9f67f8
MichalMitros <mitrosm@ee.pw.edu.pl>
2016-12-18 18:03:53 +0100

    Zrobiona funkcja alloc - prostsza wersja dla tylko 2 wierszy

--- project/alokator.c
+++ project/alokator.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void alloc(double *x, int n) {
-	if(n==0) {
-		x = malloc(sizeof(double));
-	} else {
-		void *tmp;
-		if((tmp = realloc(x, (n+1)*sizeof(double))) == 0);
-			printf("\nMemory allocation error!\n");
+
+double **alloc(double **x, int k) {
+	
+	/*	Krotka instrukcja obslugi:
+		**x - tablica 2D, ktora sobie zaalokujemy (poki co z 2 wierszami)
+		k - obecna ilosc kolumn
+		Wiecej informacji pod linkiem: https://media.giphy.com/media/ZxQNtptRbLrdm/giphy.gif
+	*/
+	
+	if(x==NULL) {
+		x = (double**) malloc(2*sizeof (double*));
+		x[0] = (double*) malloc((k+1)*sizeof (double));
+		x[1] = (double*) malloc((k+1)*sizeof (double));
+	}else {
+		x[0] = realloc(x[0],(k+1)*sizeof (double));
+		x[1] = realloc(x[1],(k+1)*sizeof (double));
 	}
+	
+	return x;
 }
 
 int main(int argc, char ** argv) {
 	double **a;
 	int i;
+	int n = argc>1 ? atoi(argv[1]) : 10;
 	
-	a = malloc(2*sizeof(double*));
 	
-	for(i=0; i<atoi(argv[1]); i++) {
-		alloc(a[0], i);
-		alloc(a[1], i);
-		a[0][i] = i;
-		a[1][i] = i+1;
-		printf("%g %g\n", a[0][i], a[1][i]);
+	for(i=0; i<n; i++) {
+		a = alloc(a, i);
+		a = alloc(a, i);
+		a[0][i] = 2*i;
+		a[1][i] = 2*i+1;
 	}
+	
+	for(i=0; i<n; i++)
+		printf("%g ", a[0][i]);
+	printf("\n");
+	for(i=0; i<n; i++)
+                printf("%g ", a[1][i]);
+	printf("\n");
 	
 	return EXIT_SUCCESS;
 }
