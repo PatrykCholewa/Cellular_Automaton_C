ceeea6d60c1d871228b292d9ad0e0e2bdc53414e
PatrykCholewa <patryk7500@gmail.com>
2017-01-15 02:58:30 +0100

    main.c - (niedziałające) czytanie flag

--- project/main.c
+++ project/main.c
@@ -1,29 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<ctype.h>
+/**
+ ** #include "struct.h"
+ ** #include "alloc.h"
+ ** #include "matrix.h"
+ ** #include "piv_ge_solver.h"
+ ** #include "czas.h"
+ ** #mod3.h"
+ ** #analizator.h"
+ ** #bisekcja.h"
+ ** #aproksymator_na_bazie.h"
+ ** #points.h"
+ ** #splines.h"
+ ** #wykreslnik.h"
+  */
 
-void alloc(double *x, int n) {
-	if(n==0) {
-		x = malloc(sizeof(double));
-	} else {
-		void *tmp;
-		if((tmp = realloc(x, (n+1)*sizeof(double))) == 0);
-			printf("\nMemory allocation error!\n");
-	}
-}
+int main(int argc, char **argv){
 
-int main(int argc, char ** argv) {
-	double **a;
-	int i;
+	int opt;
+	FILE *in;
+	FILE *out;
+	int max_iter = 1000;
+	double dt = 0.001;
+	double tempk = 200;
+	double tk = 0.4;
+	char bool3 = 0;
 	
-	a = malloc(2*sizeof(double*));
+	while ((opt = getopt(argc, argv, "ceip:")) != -1){
+		switch (opt){
+			case 'c':
+				in = fopen( optarg ,"r" );
+				break;
+			case 'i':
+				printf( "%d\n" , max_iter);
+				max_iter = atoi(optarg);
+				printf( "%d\n" , max_iter);
+				sscanf(optarg, "%lf", &dt);
+				break;
+			case 'p':
+				out = fopen( optarg , "w" );
+				break;
+			case 'e':
+				b = 1;
+				sscanf(optarg, "%lf", &tempk);
+				sscanf(optarg, "%lf", &tk);
+				break;
+			default:
+				fprinf(stderr, "Błąd oflagowania!");
+				exit(EXIT_FAILURE);
+		}
+	}	
+	/**
+	  * Nie działa, bo ma problemy z konwersją stringów. Nie wiem czemu.
+	  * Przynajmniej umie wchodzić do case'ów.
+	  */
 	
-	for(i=0; i<atoi(argv[1]); i++) {
-		alloc(a[0], i);
-		alloc(a[1], i);
-		a[0][i] = i;
-		a[1][i] = i+1;
-		printf("%g %g\n", a[0][i], a[1][i]);
+	if( in == NULL ){
+		in = fopen( "stale.cfg" , "r");
+		if ( in == NULL ){
+			fprintf (stderr , "Brak pliku wejścia.");
+			exit(EXIT_FAILURE);
+		}
+	}
+	if (out == NULL ){
+		out = fopen( "wykres.png" , "w" );
 	}
 	
-	return EXIT_SUCCESS;
-}
+	/**
+	  * ...
+	  */
+	
+	return 0;
+}	
