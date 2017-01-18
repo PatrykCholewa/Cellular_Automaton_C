f54db816cc0a8d66601f7fc7659cef036f32d86b
PatrykCholewa <patryk7500@gmail.com>
2017-01-18 11:57:01 +0100

    Wielkie kopiowanie z10. Scalony makefile (niecałkowicie działa)

--- project/aproksymator/aproksymator_na_bazie.c
+++ project/aproksymator/aproksymator_na_bazie.c
@@ -152,26 +152,17 @@ make_spl(points_t * pts, spline_t * spl)
 
 	eqs = make_matrix(nb, nb + 1);
 
-#ifdef DEBUG
 #define TESTBASE 500
 	{
 		FILE           *tst = fopen("debug_base_plot.txt", "w");
-		double		dx = (b - a) / (TESTBASE - 1);
-		for( j= 0; j < nb; j++ )
-			xfi( a, b, nb, j, tst );
+		double		dx = (b - a) / (TESTBASE - 1 ) ;
 		for (i = 0; i < TESTBASE; i++) {
-			fprintf(tst, "%g", a + i * dx);
 			for (j = 0; j < nb; j++) {
-				fprintf(tst, " %g", fi  (a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", dfi (a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", d2fi(a, b, nb, j, a + i * dx));
-				fprintf(tst, " %g", d3fi(a, b, nb, j, a + i * dx));
+				fprintf(tst, "%g %g\n", a + i * dx, fi  (a, b, nb, j, a + i * dx));
 			}
-			fprintf(tst, "\n");
 		}
 		fclose(tst);
 	}
-#endif
 
 	for (j = 0; j < nb; j++) {
 		for (i = 0; i < nb; i++)
