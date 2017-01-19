c7e4892916571b74882188f30b97abf374f67a0d
MichalMitros <mitrosm@ee.pw.edu.pl>
2017-01-20 00:06:11 +0100

    Bisekcja zyyyyje! Hurrraaaa!

--- project/bisekcja.c
+++ project/bisekcja.c
@@ -15,19 +15,6 @@ double szukaj_bisekcyjnie( cool_t t) {
 	double a = 0.001;
 	double b = 100.0;
 	double tmp = (a+b)/2;
-	printf("%g\n", wynik(t, t->stale.mw));
-	/*
-	while((wynik(t, a)-t->tend)+(wynik(t, b)-t->tend) > eps) {
-		double tmp = ((a+b)/2.0);
-		printf("a %g   b %g   tmp %g    t %g\n",a, b, tmp, wynik(t, tmp)-t->tend);
-		printf("%g\n", t->stale.mw);
-		if(wynik(t, tmp)-t->tend == 0)
-			return tmp;
-		else if(wynik(t, tmp)-t->tend < 0)
-			b = tmp;
-		else
-			a = tmp;
-	}*/
 
 	while(wynik(t, tmp) > t->tend+eps || wynik(t, tmp) < t->tend-eps) {
 		if(wynik(t, tmp) < t->tend)
@@ -35,10 +22,8 @@ double szukaj_bisekcyjnie( cool_t t) {
 		else
 			b = tmp;
 		tmp = (a+b)/2;
-		printf("a=%g b=%g sr=%g wyn=%g\n",a,b,tmp, wynik(t, tmp));
 	}
 	
-	printf("Temp = %g\n", t->Y[1][t->Yc]);
 	
 	return tmp;
 }
