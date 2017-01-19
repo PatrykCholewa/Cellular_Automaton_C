ede8eae74db3269491e1ba2137b125d675d0a1e4
PatrykCholewa <patryk7500@gmail.com>
2017-01-19 16:07:47 +0100

    Test.

--- project/czas.c
+++ project/czas.c
@@ -10,9 +10,9 @@ cool_t przebiegnij( cool_t cool_data){
 
 	for(i = 1 ; i <= cool_data->Yc ; i++){
 
-	cool_data->Y[0][i] = cool_data->Y[0][i - 1] + cool_data->stale.h * ( ( cool_data->Y[1][i - 1] - cool_data->Y[0][i - 1]) * cool_data->stale.h * cool_data->stale.A / cool_data->stale.mb / cool_data->stale.cb );
+	cool_data->Y[0][i] = cool_data->Y[0][i - 1] + cool_data->dt * ( ( cool_data->Y[1][i - 1] - cool_data->Y[0][i - 1]) * cool_data->dt * cool_data->stale.A / cool_data->stale.mb / cool_data->stale.cb );
 
-	cool_data->Y[1][i] = cool_data->Y[1][i - 1] + cool_data->stale.h * ( ( cool_data->Y[0][i - 1] - cool_data->Y[1][i - 1]) * cool_data->stale.h * cool_data->stale.A / cool_data->stale.mw / cw(cool_data) );
+	cool_data->Y[1][i] = cool_data->Y[1][i - 1] + cool_data->dt * ( ( cool_data->Y[0][i - 1] - cool_data->Y[1][i - 1]) * cool_data->dt * cool_data->stale.A / cool_data->stale.mw / cw(cool_data) );
 
 	}	
 
