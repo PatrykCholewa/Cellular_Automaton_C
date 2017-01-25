82e4175e63e9fd72bebd4c2a6b2cda38aca5b21b
PatrykCholewa <patryk7500@gmail.com>
2017-01-25 11:07:51 +0100

    Konspekt konektora.

---
+++ project/konektor.c
@@ -0,0 +1,6 @@
+#include<struct.h>
+#include<splines.h>
+
+void spl_gen( cool_t cool_data , char *cwfilename ){
+
+}
---
+++ project/konektor.h
@@ -0,0 +1,3 @@
+#include<struct.h>
+
+void spl_gen( cool_t cool_data , char *cwfilename );
