4d23314bb9b2a9dd47fc5c790e87ba19783dc98d
PatrykCholewa <patryk7500@gmail.com>
2017-03-13 22:18:25 +0100

    Error commit

--- cell/src/main.c
+++ cell/src/main.c
@@ -5,32 +5,25 @@
 //#include"gen.h"
 
 int main(int argc , char **argv ){
-	map_t map;
-	FILE *in_set = NULL;
-	FILE *in_map = NULL;
 
-	in_set = argc > 2 ? fopen( argv[2] , "r" ) : NULL;
-	in_map = argc > 1 ? fopen( argv[1] , "r" ) : NULL;
+	map_t map;
+
+	FILE *in_map = argc > 1 ? fopen( argv[1] , "r" ) : fopen( "./bin/map.cfg" , "r" );
+	FILE *in_set = argc > 2 ? fopen( argv[2] , "r" ) : fopen( "./bin/set.cfg" , "r" );
 
 	if( in_set == NULL ){
-		in_set = fopen( "./bin/set.cfg" , "r" );
-		if ( in_set == NULL ){
-			fprintf ( stderr , "File reading error!\n");
-			exit( EXIT_FAILURE );
-		}
+		fprintf ( stderr , "File reading error!\n");
+		exit( EXIT_FAILURE );
 	}
 	
 	if( in_map == NULL ){
-		in_map = fopen( "./bin/map.cfg" , "r" );
-		if ( in_map == NULL ){
-			fprintf ( stderr , "File reading error!\n");
-			exit( EXIT_FAILURE );
-		}
+		fprintf ( stderr , "File reading error!\n");
+		exit( EXIT_FAILURE );
 	}
-	
+
 	map = initalloc( map );
 	map = add_cfg( map , in_set );
-	map = boardalloc( map );
+//	map = boardalloc( map );
 	//map = add_map( map );
 
 	fclose( in_set );
