37c56c00840d9b9f128afd9c579b599097ae3f12
PatrykCholewa <patryk7500@gmail.com>
2017-03-21 16:51:44 +0100

    Working, but not fixed. :(

--- cell/src/main.c
+++ cell/src/main.c
@@ -4,19 +4,20 @@
 #include"alloc.h"
 //#include"gen.h"
 
+#define FILE_NAME_SIZE 16
+
 int main(int argc , char **argv ){
 
 	map_t map;
-
-	FILE *in_map = argc > 1 ? fopen( argv[1] , "r" ) : fopen( "./bin/map.cfg" , "r" );
+//	FILE *in_map = argc > 1 ? fopen( argv[1] , "r" ) : fopen( "./bin/map.cfg" , "r" );
 	FILE *in_set = argc > 2 ? fopen( argv[2] , "r" ) : fopen( "./bin/set.cfg" , "r" );
 
+//	if( in_map == NULL ){
+//		fprintf ( stderr , "File reading error!\n");
+//		exit( EXIT_FAILURE );
+//	}
+
 	if( in_set == NULL ){
-		fprintf ( stderr , "File reading error!\n");
-		exit( EXIT_FAILURE );
-	}
-	
-	if( in_map == NULL ){
 		fprintf ( stderr , "File reading error!\n");
 		exit( EXIT_FAILURE );
 	}
@@ -27,11 +28,12 @@ int main(int argc , char **argv ){
 	//map = add_map( map );
 
 	fclose( in_set );
-	fclose( in_map );
+//	fclose( in_map );
 	
 	//map = iter_gen( map );
 
 	//freealloc( map );
+
 	return 0;
 
 }
