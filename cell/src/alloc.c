a679989526aca4b2b0133db308616ea64f15d6f1
Patryk Cholewa <patryk.cholewa.S@ee.pw.edu.pl>
2017-04-04 12:32:29 +0200

    Reading sequence does not matter now. (Pointer fix in alloc.c)

--- cell/src/alloc.c
+++ cell/src/alloc.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 
-#define MAX_FILENAME_SIZE 1024
+#define MAX_FILENAME_SIZE 128
 #define BUF_SIZE 1024
 
 map_t initalloc (map_t map ){
@@ -54,45 +54,52 @@ map_t add_cfg( map_t map , FILE *in ){
 		if( strcmp( s , "nbrhd" ) == 0 ){
 			if( ( strcmp( v , "Moore") == 0 ) || ( strcmp(  v , "8" ) == 0 ) ){
 				map->cfg.nbrhd = 8;
-			}
+			} else
 			if( ( strcmp( v , "Neumann") == 0 )  || ( strcmp( v , "4" ) == 0 ) ){
 				map->cfg.nbrhd = 4;
 			}
 			/*
 				Custom
 			*/
-		}
+		} else
 		if( strcmp( s , "bound" ) == 0 ){
 			if( strcmp( v , "1" ) == 0 ){
 				map->cfg.bound = 1;
-			}
+			} else
 			if( strcmp( v , "0" ) == 0 ){
 				map->cfg.bound = 0;
-			}
+			} else
 			if( ( strcmp( v , "-1" ) == 0 ) || ( strcmp( v , "infinite" ) == 0 )  ){
 				map->cfg.bound = -1;
 			}
-		}		
+		} else	
 		if( strcmp( s , "save" ) == 0 ){
 			map->cfg.save = atoi(v);
-		}
+		} else
 		if( strcmp( s , "intrvl" ) == 0 ){
 			map->cfg.intrvl = atoi(v);
-		}
+		} else
 		if( strcmp( s , "out" ) == 0 ){
-			map->cfg.out = v;
-		}	
+			int iter;
+			map->cfg.out = malloc( MAX_FILENAME_SIZE * sizeof( map->cfg.out ) );
+			for( iter = 0 ; iter < MAX_FILENAME_SIZE ; iter++ ){
+				map->cfg.out[iter] = v[iter];
+				if( v[iter] == '\0' ){
+					iter = MAX_FILENAME_SIZE;
+				}
+			}
+		} else	
 		if( strcmp( s , "m" ) == 0 ){
 			map->m = atoi(v);
-		}
+		} else
 		if( strcmp( s , "n" ) == 0 ){
 			map->n = atoi(v);
-		}
+		} else 
 		if( strcmp( s , "live" ) == 0 ){
 			if( strcmp( v , "yes" ) == 0 ){
 				map->cfg.live = 1;				
 			}
-		}
+		} else {}
 	
 	}
 
@@ -124,10 +131,9 @@ void freealloc( map_t map ){
 		free( map->board[i] );
 		free( map->old_board[i] );
 	}
-	
+	free( map->cfg.out );
 	free( map->board );
 	free( map->old_board );
-//	free( map->cfg.out );	
 	free( map );
 	
 	return ;
